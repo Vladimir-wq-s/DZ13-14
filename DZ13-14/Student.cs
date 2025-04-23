5b5ef1757c30dc2649ff1f4345907454099db753
Vladimir-wq-s <midomipar81@gmail.com>
2025-04-23 13:56:19 +0700

    Добавьте файлы проекта.

---
+++ DZ13-14/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace StudentManagement
+{
+    //Класс, представляющий студента
+    public class Student
+    {
+        //Свойство для имени студента
+        public string Name { get; set; }
+
+        //Свойство для возврата студента
+        public int Age { get; set; }
+
+        //Свойство для среднего балла студента
+        public double AverageScore { get; set; }
+
+        //Конструктор класса студента
+        public Student(string name, int age, double averageScore)
+        {
+        Name = name;
+            Age = age;
+            AverageScore = averageScore;
+
+}
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace StudentManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создание списка студентов
+            List<Student> students = new List<Student>
+            {
+                new Student ("Alice", 22,3.8),
+                new Student ("Vladimir", 19,3.2),
+                 new Student ("Sergey", 23,3.6),
+                  new Student ("Sasha", 21,4.0),
+                   new Student ("Alexey", 20,3.4),
+
+            };
+            // LINQ для фильтрации студентов
+            var filterStudents = from student in students
+                                 where student.Age > 20 && student.AverageScore > 3.5
+
+                                 select student;
+            // Вывод этих отфильтрованных студентов
+            Console.WriteLine("Студенты старше 20 лет с средним баллом выше 3.5");
+            foreach (var student in filterStudents) 
+            {
+                Console.WriteLine($"{student.Name}, Возвраст: {student.Age}, Средний балл: {student.AverageScore}");
+            }
+
+
+        }
+    }
+}
